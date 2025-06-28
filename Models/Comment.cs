fd70c3867d4f283e7ead01ef12ba005dfc1dcefc
devincar <devmcaron@gmail.com>
2025-06-28 09:46:54 -0700

    Add project files.

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace Finshark_api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        // Navigation Property
+        public Stock? Stock { get; set; }
+    }
+}
