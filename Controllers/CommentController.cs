18c34218abd86537e419ee030873f7c12be50330
devincar <devmcaron@gmail.com>
2025-07-19 09:55:01 -0700

    Added comment Update

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -51,7 +51,21 @@ namespace Finshark_api.Controllers
 
             var commentModel = commentDto.ToCommentFromCreate(stockId);
             await _commentRepo.CreateAsync(commentModel);
-            return CreatedAtAction(nameof(GetById), new { id = commentModel }, commentModel.ToCommentDto());
+            return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+
+            if (comment == null)
+            {
+                return NotFound("Comment Not Found");
+            }
+
+            return Ok(comment.ToCommentDto());
         }
     }
 }
--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -30,5 +30,21 @@ namespace Finshark_api.Repository
         {
             return await _context.Comments.FindAsync(id);
         }
+
+        public async Task<Comment> UpdateAsync(int id, Comment comment)
+        {
+            var existingComment = await _context.Comments.FindAsync(id);
+
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            existingComment.Title = comment.Title;
+            existingComment.Content = comment.Content;
+
+            await _context.SaveChangesAsync();
+            return existingComment;
+        }
     }
 }
